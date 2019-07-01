5fedc4203e4bb38dd030791b5dc7fa17133f6cd6
Andrii Korzh <korzh.andrii@gmail.com>
2019-07-01 14:45:22 -0700

    alt for profilepicture

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ class About extends Component {
 				<div className="space-160"/>
 				<div className="row">
 					<div className="three columns">
-						<img className="profile-pic" src={profilepic} alt="Andrii Korzh Profile Pic"/>
+						<img className="profile-pic" src={profilepic} alt="Iuliia Kudriashova Profile Pic"/>
 					</div>
 					<div className="nine columns main-col">
 						<h2>About Me</h2>
