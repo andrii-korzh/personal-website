023c7af77092b59b871c5449de2ea371b3f2e1ff
Andrii Korzh <korzh.andrii@gmail.com>
2019-07-01 11:56:32 -0700

    update profile pic

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ class About extends Component {
 				<div className="space-160"/>
 				<div className="row">
 					<div className="three columns">
-						{/*TODO<img className="profile-pic" src={profilepic} alt="Andrii Korzh Profile Pic"/>*/}
+						<img className="profile-pic" src={profilepic} alt="Andrii Korzh Profile Pic"/>
 					</div>
 					<div className="nine columns main-col">
 						<h2>About Me</h2>
