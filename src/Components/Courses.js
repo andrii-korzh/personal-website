992c36417ed5ee637b5f1751218704de41d07364
Andrii Korzh <korzh.andrii@gmail.com>
2019-09-04 13:05:58 -0700

    [PW-8]: Add courses certificates(concurrency java)

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -2,13 +2,17 @@ import React, {Component} from 'react';
 
 const imgHome = "images/course";
 
-const name = "coursera-parallel-programming-java.png";
-const url = "https://www.coursera.org/account/accomplishments/certificate/ASXVSAQ7EHL7";
-const description = "Coursera Parallel Programming Java Certificate";
+const nameParallelJava = "coursera-parallel-programming-java.png";
+const urlParallelJava = "https://www.coursera.org/account/accomplishments/certificate/ASXVSAQ7EHL7";
+const descriptionParallelJava = "Coursera Parallel Programming Java Certificate";
+
+const nameConcurrentJava = "coursera-concurrent-programming-java.png";
+const urlConcurrentJava = "https://www.coursera.org/account/accomplishments/records/R472UNFTQQXF";
+const descriptionConcurrentJava = "Coursera Concurrent Programming Java Certificate";
 
 const urlReact = "https://www.udemy.com/certificate/UC-NG2NYT1J/";
 const nameReact = "react.png";
-const descriptionReact = "Coursera Parallel Programming Java Certificate";
+const descriptionReact = "Udemy React 16 - The Complete Guide(incl. React Router 4 & Redux)";
 
 const urlDocker = "https://www.udemy.com/certificate/UC-UK3Z5MK5/";
 const nameDocker = "docker.png";
@@ -17,7 +21,6 @@ const descriptionDocker = "Udemy - Docker Mastery: The Complete Toolset From a D
 const urlBash = "https://www.udemy.com/certificate/UC-DXUC7S3E/";
 const nameBash = "bash.png";
 const descriptionBash = "Udemy - BASH Programming Course: Master the Linux Command Line!";
-
 
 const idPrefix = "certs-";
 let lastId = 0;
@@ -32,7 +35,8 @@ class Certificates extends Component {
 		return (
 			<section style={center}>
 				<ul>
-					{this.getCertificate(url, url, imgHome + "/" + name, description)}
+					{this.getCertificate(urlParallelJava, urlParallelJava, imgHome + "/" + nameParallelJava, descriptionParallelJava)}
+					{this.getCertificate(urlConcurrentJava, urlConcurrentJava, imgHome + "/" + nameConcurrentJava, descriptionConcurrentJava)}
 					{this.getCertificate(urlReact, urlReact, imgHome + "/" + nameReact, descriptionReact)}
 					{this.getCertificate(urlDocker, urlDocker, imgHome + "/" + nameDocker, descriptionDocker)}
 					{this.getCertificate(urlBash, urlBash, imgHome + "/" + nameBash, descriptionBash)}
